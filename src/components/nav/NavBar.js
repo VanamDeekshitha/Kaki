525ca94b19bb898f00a6c1e4aadca8880eebe8ac
VanamDeekshitha <vanamdeekshitha@gmail.com>
2024-07-24 15:04:16 +0530

    head

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -5,6 +5,7 @@ const NavBar = () => {
     <div className="nav">
       {/* <img src={logo} alt="Logo" className="logo" width={50} /> */}
       <p>logo</p>
+      <h1>Kaki</h1>
     </div>
   );
 };
