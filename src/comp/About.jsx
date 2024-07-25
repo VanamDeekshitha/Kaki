8d78d6384cf607117f14a46ad6cef0594ab0416c
VanamDeekshitha <vanamdeekshitha@gmail.com>
2024-07-25 14:39:07 +0530

    Added browser routing

---
+++ src/comp/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./comp.css";
+
+function About() {
+  return (
+    <div >
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident non totam quis assumenda sequi similique velit expedita ducimus hic modi sed, rem ipsa eum, dicta laudantium illum voluptas necessitatibus mollitia!</p>
+    </div>
+  )
+}
+
+export default About
